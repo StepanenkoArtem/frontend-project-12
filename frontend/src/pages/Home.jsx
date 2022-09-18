fd0f88af22c948688057360c007943d7be9b9292
Artem Stepanenko <astepanenko@cybercraftinc.com>
2022-09-18 18:12:46 +0300

    add Home and Not Found pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/login">Login</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Home;
