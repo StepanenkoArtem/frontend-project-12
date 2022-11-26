0ae4178cf1dce6426adabfb68056ac2c9bed4138
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2022-11-26 18:26:55 +0200

    update signUp validation scheme

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -10,8 +10,8 @@ import { useCurrentUser } from '../contexts/CurrentUser';
 import Header from './Header';
 
 const signUpSchema = Yup.object({
-  username: Yup.string().required(),
-  password: Yup.string().required(),
+  username: Yup.string().min(3).required(),
+  password: Yup.string().min(6).required(),
   passwordConfirmation: Yup.string()
     .when('password', (password, field) => (password ? field.required().oneOf([Yup.ref('password')]) : field)),
 });
