b435ca9a4257e26c254af71f953daaf11a160418
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2022-11-30 19:15:30 +0200

    small refactoring for 404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Header from './Header';
 
-const NotFound = () => (
+const NotFound = () => {
+  const { t } = useTranslation();
+  return (
 
-  <div className="d-flex flex-column h-100">
-    <Header />
-    <div>PAGE NOT FOUND</div>
-  </div>
-);
+    <div className="d-flex flex-column h-100">
+      <Header />
+      {t('pageNotFound.title')}
+      {t('PageNotFound.text')}
+    </div>
+  );
+};
 
 export default NotFound;
