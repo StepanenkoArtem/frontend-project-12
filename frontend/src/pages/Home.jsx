f515ffde863d4e64aadae224c66c79bb1ef8d890
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2022-12-01 16:56:45 +0200

    rewrite home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
 import Chat from './chat/Chat';
-import { useCurrentUser } from '../contexts/CurrentUser';
-import Header from './Header';
+import Header from '../commonComponents/Header';
 
-const Home = () => {
-  const { currentUser } = useCurrentUser();
-
-  return (currentUser
-    ? (
-      <div className="d-flex flex-column h-100">
-        <Header />
-        <Chat />
-      </div>
-    )
-    : <Navigate to="login" />
-  );
-};
+const Home = () => (
+  <div className="d-flex flex-column h-100">
+    <Header />
+    <Chat />
+  </div>
+);
 
 export default Home;
