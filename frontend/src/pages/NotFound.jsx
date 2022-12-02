14ce80a5b437bb5d7248470d5bbab5231ffa4040
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2022-12-02 16:50:43 +0200

    fix not found

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import Header from '../commonComponents/Header';
 
 const NotFound = () => {
   const { t } = useTranslation();
   return (
-    <>
+    <div className="container h-100">
       <Header />
-      <div className="d-flex flex-column h-100">
-        {t('pageNotFound.title')}
-        {t('PageNotFound.text')}
+      <div className="d-flex flex-column text-center my-auto">
+        <h2>{t('pageNotFound.title')}</h2>
+        <p className="text-center">
+          <span>{t('pageNotFound.go')}</span>
+          {' '}
+          <Link to="/">{t('goHome')}</Link>
+        </p>
+
       </div>
-    </>
+    </div>
   );
 };
 
