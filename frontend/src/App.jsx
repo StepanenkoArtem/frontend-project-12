8c7c6075791a40f28961553ca1b7f9ccb9c54510
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2022-12-13 20:43:06 +0200

    move redirection login to the routeng

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,27 +2,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React from 'react';
 import {
-  BrowserRouter as Router, Navigate, Route, Routes,
+  BrowserRouter as Router, Route, Routes,
 } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 import SignUp from './pages/SignUp/SignUp';
-import { useCurrentUser } from './contexts/CurrentUser';
+import PublicRoutes from './utils/PublicRoutes';
+import PrivateRoutes from './utils/PrivateRoutes';
 
-const App = () => {
-  const { currentUser } = useCurrentUser();
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={currentUser ? <Home /> : <Navigate to="login" />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={currentUser ? <Navigate to="/" /> : <SignUp />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Routes>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<Home />} exact />
+      </Route>
+      <Route element={<PublicRoutes />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
